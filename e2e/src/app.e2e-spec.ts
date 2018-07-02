e73e25dc7d346cc31cb44ef0f4f703ebca74bdf7
ytflzq <1057723169@qq.com>
2018-07-02 23:11:28 +0800

    更新node版本后，重新新建项目，增加ng-zorro-antd

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('myproject App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('myproject App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to myproject!');
   });
 });
